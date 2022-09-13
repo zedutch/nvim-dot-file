959fa7f17ca98e9a42f0c26806fc785cf4c79475
Robin Arys <robin@arys.dev>
2022-09-13 09:35:31 +0200

    Disable null-ls eslint server since a dedicated lsp server exists

--- lua/cfg/null-ls.lua
+++ lua/cfg/null-ls.lua
@@ -1,7 +1,10 @@
 local null_ls = require("null-ls")
 
+---- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
+---- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
+---- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/code_actions
 local code_actions = null_ls.builtins.code_actions
 null_ls.setup {
     debug = false,
@@ -24,7 +27,8 @@ null_ls.setup {
         },
 
         -- Javascript
-        diagnostics.eslint_d,
+        -- diagnostics.eslint_d,
+        -- code_actions.eslint_d,
         diagnostics.tsc,
 
         -- CSS
