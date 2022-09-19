560a979c070bba10f3eb2d7f026961112f5d1226
Robin Arys <robin@arys.dev>
2022-09-19 08:04:29 +0200

    Ignore ClubList subprojects

--- lua/settings/plugins/project.lua
+++ lua/settings/plugins/project.lua
@@ -14,7 +14,7 @@ project.setup {
     -- lsp, while **"pattern"** uses vim-rooter like glob pattern matching. Here
     -- order matters: if one is not detected, the other is used as fallback. You
     -- can also delete or rearangne the detection methods.
-    detection_methods = { "!>projects", "pattern", "lsp" },
+    detection_methods = { "!>projects", "!>ClubList", "pattern", "lsp" },
 
     -- All the patterns used to detect root dir, when **"pattern"** is in
     -- detection_methods
