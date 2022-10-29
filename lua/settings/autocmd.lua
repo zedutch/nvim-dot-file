37917f045417720c90bbcabeac0ed3a73c687bd2
Robin Arys <robin@arys.dev>
2022-10-29 09:30:59 +0200

    Disable eslint autofix since it has issues on macOS

--- lua/settings/autocmd.lua
+++ lua/settings/autocmd.lua
@@ -2,8 +2,8 @@ local api = vim.api
 
 api.nvim_create_augroup('my-ESLint', { clear = true })
 
-api.nvim_create_autocmd('BufWritePre', {
-    group = 'my-ESLint',
-    pattern = { "*.tsx", "*.ts", "*.jsx", "*.js" },
-    command = 'EslintFixAll',
-})
+-- api.nvim_create_autocmd('BufWritePre', {
+--     group = 'my-ESLint',
+--     pattern = { "*.tsx", "*.ts", "*.jsx", "*.js" },
+--     command = 'EslintFixAll',
+-- })
