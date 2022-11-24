227cc652e09f5fb39d9d7eb483fa24d78facc8ab
Robin Arys <robin@arys.dev>
2022-11-24 09:12:09 +0100

    Disable 3rd party checks in lua

--- lua/settings/lsp/sumneko_lua.lua
+++ lua/settings/lsp/sumneko_lua.lua
@@ -6,6 +6,7 @@ return {
             },
             workspace = {
                 library = vim.api.nvim_get_runtime_file('', true),
+                checkThirdParty = false,
             },
             telemetry = {
                 enable = false,
