6583c23f332a378671f88f283c0d1efcab8c1b2d
Robin Arys <robin@arys.dev>
2023-04-17 08:46:37 +0200

    Tell lsp-zero to skip rust analyzer in favour of rust-tools

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -165,6 +165,8 @@ end)
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
+lsp.skip_server_setup({ 'rust_analyzer' })
+
 lsp.setup()
 
 vim.diagnostic.config {
