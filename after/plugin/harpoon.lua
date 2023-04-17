0fb73afa76398c92730e44930b1c01bda93b7856
Robin Arys <robin@arys.dev>
2023-04-17 08:54:01 +0200

    Add window resizing keymaps back

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -3,9 +3,9 @@ local ui = require('harpoon.ui')
 
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-j>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-k>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-l>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<leader>hh", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<leader>hj", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<leader>hk", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<leader>hl", function() ui.nav_file(4) end)
 
 require('telescope').load_extension('harpoon')
