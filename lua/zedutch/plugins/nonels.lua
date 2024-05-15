1839a020da3ca8d4358ec2c652f021a439ddc3c5
Robin Arys <robin@arys.dev>
2024-05-15 09:33:10 +0200

    Format templ using prettier through none-ls

--- lua/zedutch/plugins/nonels.lua
+++ lua/zedutch/plugins/nonels.lua
@@ -32,6 +32,7 @@ return {
                         "typescript",
                         "typescriptreact",
                         "htmldjango",
+                        "templ",
                     },
                 }),
 
