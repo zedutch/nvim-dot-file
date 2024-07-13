a2f567f8dae6650418f08d20ffb258b73f593d76
Robin Arys <robin@arys.dev>
2024-07-13 16:30:15 +0200

    Update trouble

--- lua/zedutch/plugins/trouble.lua
+++ lua/zedutch/plugins/trouble.lua
@@ -3,12 +3,10 @@ return {
     "folke/trouble.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     keys = {
-        { "<leader>tt", "<cmd>TroubleToggle<cr>" },
-        { "<leader>tr", "<cmd>TroubleRefresh<cr>" },
-        -- { "<leader>tj", function() require("trouble").next({ skip_groups = true, jump = true }) end },
-        -- { "<leader>tk", function() require("trouble").previous({ skip_groups = true, jump = true }) end },
-        { "L",  function() require("trouble").next({ skip_groups = true, jump = true }) end },
-        { "H",  function() require("trouble").previous({ skip_groups = true, jump = true }) end },
+        { "<leader>tt", "<cmd>Trouble diagnostics toggle<cr>" },
+        { "<leader>td", "<cmd>Trouble todo toggle<cr>" },
+        { "L",          function() require("trouble").next({ skip_groups = true, jump = true }) end },
+        { "H",          function() require("trouble").previous({ skip_groups = true, jump = true }) end },
     },
     opts = {
         mode = "workspace_diagnostics",
