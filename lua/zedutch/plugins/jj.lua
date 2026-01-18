8a06335b875364cddf649d6dc1c339701ab43dc4
Robin Arys <robin@arys.dev>
2026-01-18 10:55:49 +0100

    Use a more advanced jj plugin

--- lua/zedutch/plugins/jj.lua
+++ lua/zedutch/plugins/jj.lua
@@ -1,10 +1,15 @@
 return {
+    -- I don't think I still need this with jj-diffconflicts?
+    -- {
+    --     -- https://github.com/Spiegie/jj-conflict-highlight.nvim
+    --     'Spiegie/jj-conflict-highlight.nvim',
+    --     version = "*",
+    --     config = function()
+    --         require("jj_conflict_highlight").setup({})
+    --     end,
+    -- },
     {
-        -- https://github.com/Spiegie/jj-conflict-highlight.nvim
-        'Spiegie/jj-conflict-highlight.nvim',
-        version = "*",
-        config = function()
-            require("jj_conflict_highlight").setup({})
-        end,
+        -- https://github.com/rafikdraoui/jj-diffconflicts
+        'rafikdraoui/jj-diffconflicts',
     },
 }
