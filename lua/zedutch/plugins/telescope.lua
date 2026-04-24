446290bfbde294b460c3ee43ee3619bf706ad5ab
Robin Arys <robin@arys.dev>
2026-04-24 09:45:38 +0200

    Use telescope master branch

--- lua/zedutch/plugins/telescope.lua
+++ lua/zedutch/plugins/telescope.lua
@@ -1,7 +1,6 @@
 return {
     -- https://github.com/nvim-telescope/telescope.nvim
     'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
     dependencies = {
         "nvim-lua/plenary.nvim",
         "sharkdp/fd",
